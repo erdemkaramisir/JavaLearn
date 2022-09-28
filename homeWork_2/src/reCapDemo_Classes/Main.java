50324a4ddde5f8badbb453da23cd569206e51de5
erdemkaramisir <83616595+erdemkaramisir@users.noreply.github.com>
2022-09-28 18:20:56 +0300

    Test değişikiliği

--- homeWork_2/src/reCapDemo_Classes/Main.java
+++ homeWork_2/src/reCapDemo_Classes/Main.java
@@ -8,7 +8,9 @@ public class Main {
         System.out.println(dortIslem.topla(52, 36));    
         System.out.println(dortIslem.cikar(154, 87));    
         System.out.println(dortIslem.carp(56, 87));    
-        System.out.println(dortIslem.bol(100, 4));    
+        System.out.println(dortIslem.bol(100, 4)); 
+        
+        System.out.println(" Dört İşlem");
         
 
     }
