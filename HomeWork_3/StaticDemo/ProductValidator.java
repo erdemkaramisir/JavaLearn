ab37856e4e68d3aa1de2a6f210b9f2f054d4c7f7
erdemkaramisir <erdemkaramisir@gmail.com>
2022-10-12 00:34:11 +0300

    tut 5

--- HomeWork_3/StaticDemo/ProductValidator.java
+++ HomeWork_3/StaticDemo/ProductValidator.java
@@ -3,7 +3,7 @@ package HomeWork_3.StaticDemo;
 public class ProductValidator {
     
     public boolean isValid(Product product) {
-        if (product.price < 0 && !product.name.isEmpty()) {
+        if (product.price > 0 && !product.name.isEmpty()) {
             return true;
         } else {
             return false;
